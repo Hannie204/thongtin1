b3d94b415b9fdb0e651683e7863fce97ab26ab91
phuocan PC <phuocantranthai@gmail.com>
2023-04-14 22:18:06 +0700

    First commit

--- src/index.js
+++ src/index.js
@@ -3,38 +3,32 @@ import fetch from 'node-fetch';
 import path from 'path';
 const app = express();
 
-// Webhook URL
 const webhookURL = "https://discord.com/api/webhooks/1089726987104428062/AnZ-7kUxSVKBq3Yo6eqlsWc3gCHn243zfFadgC5ZDKrfbbfuT5y_7vyq_Mlji5x4a11v";
 
-// Serve static files
 app.use(express.static('public'));
 
-// Parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
 
-// Handle GET request for homepage
 app.get('/', (req, res) => {
   res.sendFile(path.resolve('public/index.html'));
 });
 
-// Handle POST request for form submission
 app.post('/send_data', async (req, res) => {
   const name = req.body.name;
   const age = req.body.age;
 
-  // Create message embed
+  //  message embed
   const embed = {
     "title": "Thông tin người dùng",
     "description": `Tên: ${name}\nTuổi: ${age}`,
-    "color": 16711680 // Red color
+    "color": 16711680 // Red
   };
 
-  // Create payload with message embed
   const payload = {
     "embeds": [embed]
   };
 
-  // Send webhook
+  // Send 
   try {
     const response = await fetch(webhookURL, {
       method: "POST",
@@ -53,7 +47,6 @@ app.post('/send_data', async (req, res) => {
   }
 });
 
-// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
